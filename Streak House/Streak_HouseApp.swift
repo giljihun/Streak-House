cbec90b838ad9d156c4e1a5acaeae5c2f37634c7
giljihun <accc45@outlook.com>
2025-04-16 21:57:15 +0900

    Initial Commit

---
+++ Streak House/Streak_HouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Streak_HouseApp.swift
+//  Streak House
+//
+//  Created by 길지훈 on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Streak_HouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
